7dcd02b23b43d5dc1ed4259e3bb5228e7d002ff8
Tomas <tomibudeguer20@gmail.com>
2022-09-22 22:27:58 -0300

    agregue el props para que aparezca el nombre de la tarea en la lista

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,18 +1,36 @@
 import ListaTarea from "./ListaTarea";
-import {Form, Button} from "react-bootstrap"
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  // aca va la mayoria de la logica
+  const [tarea, setTarea] = useState("");
+  const [arregloTarea, setArregloTarea] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // no se puede hacer el .push directamente
+    setArregloTarea([...arregloTarea, tarea]);
+    // limpiar el input
+    setTarea('');
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
-        <Button variant="primary" type="submit">
-          Enviar
-        </Button>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
-      <ListaTarea></ListaTarea>
+      <ListaTarea arregloTarea={arregloTarea} ></ListaTarea>
     </div>
   );
 };
--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,9 @@
 import { ListGroup, Button } from "react-bootstrap";
 
-const ItemTarea = () => {
+const ItemTarea = (props) => {
   return (
     <ListGroup.Item className="d-flex justify-content-between">
-      Tarea de prueba
+      {props.nombreTarea}
       <Button variant="danger">Borrar</Button>
     </ListGroup.Item>
   );
